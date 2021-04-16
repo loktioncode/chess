64f8776b638788ac174495f4c885a1ee8e13aadc
loktioncode <elishabere4@gmail.com>
2021-04-16 15:51:25 +0200

    add position class

--- chess.js
+++ chess.js
@@ -1,25 +1,78 @@
 
-
-class Piece{
-    constructor(
-        position = 1
-    )
-
-
+class Piece {
+  
+    constructor(){
+        this.killed = false;
+        this.white = false;
+    }
+  
+     Piece(white)
+    {
+        this.setWhite(white);
+    }
+  
+    isWhite()
+    {
+        return this.white;
+    }
+  
+   setWhite( white)
+    {
+        this.white = white;
+    }
+  
+    isKilled()
+    {
+        return this.killed;
+    }
+  
+   setKilled( killed)
+    {
+        this.killed = killed;
+    }
+  
+    canMove(board, start, end);
 }
 
-class Position{
-    constructor(x, y
-    ){
-        this.x = y,
-        this.y = y
+class Spot {
+
+  
+   constructor(x, y, piece)
+    {
+        this.setPiece(piece);
+        this.setX(x);
+        this.setY(y);
     }
-
-    getPieces(){
-        return this
+  
+   getPiece()
+    {
+        return this.piece;
     }
-
-
+  
+  setPiece(p)
+    {
+        this.piece = p;
+    }
+  
+   getX()
+    {
+        return this.x;
+    }
+  
+    setX(x)
+    {
+        this.x = x;
+    }
+  
+   getY()
+    {
+        return this.y;
+    }
+  
+   setY(y)
+    {
+        this.y = y;
+    }
 }
 
 
