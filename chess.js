4aa69c9c5ba7cc5b656c22305aedffc5254a6c1f
DESKTOP-TEK06LE\EBere <elisha.b@cestasoft.co.za>
2021-04-16 15:43:29 +0200

    first commit

---
+++ chess.js
@@ -0,0 +1,28 @@
+
+
+class Piece{
+    constructor(
+        position = 1
+    )
+
+
+}
+
+class Position{
+    constructor(x, y
+    ){
+        this.x = y,
+        this.y = y
+    }
+
+    getPieces(){
+        return this
+    }
+
+
+}
+
+
+
+pawn = new Piece();
+bishop = new Piece();
